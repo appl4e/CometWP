ce42020a41241df14854c14c1048bf75f50dfb94
apple <apple@redlimesolutions.com>
2018-08-15 11:27:13 +0600

    created different template files for different post types

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ get_header();
         <div class="blog-posts">
         <?php if(have_posts()): ?>
           <?php while(have_posts()): the_post(); ?>
-            <?php get_template_part('template-parts/content'); ?>
+            <?php get_template_part('template-parts/content', get_post_format()); ?>
           <?php endwhile; ?>
         <?php endif; ?>
         </div>
---
+++ template-parts/content-video.php
@@ -0,0 +1,20 @@
+<article class="post-single">
+	<div class="post-info">
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<h6 class="upper">
+		<span>By</span><a href="<?php echo get_template_directory_uri(); ?>/author/<?php the_author(); ?>"> <?php the_author(); ?></a>
+		<span class="dot"></span>
+		<span><?php the_time('d F Y'); ?></span>
+		<span class="dot"></span>
+		<a href="#" class="post-tag"><?php the_tags(); ?></a></h6>
+	</div>
+	<div class="post-media">
+        <div class="media-video">
+            <iframe src="https://www.youtube.com/embed/rrT6v5sOwJg" frameborder="0"></iframe>
+        </div>
+    </div>
+	<div class="post-body">
+	<p><?php echo wp_trim_words(get_the_content(), 50, ''); ?></p>
+	<p><a href="<?php the_permalink(); ?>" class="btn btn-color btn-sm">Read More</a></p>
+	</div>
+</article>
